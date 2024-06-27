1a5c5a8aab6cc205174e68a44fdcda0c9c4bc447
yeosang0508 <yeosang0508@gmail.com>
2024-06-28 00:36:59 +0900

    종료 기능 구현, 등록 기능 구현중

---
+++ src/main/java/org/koreait/Main.java
@@ -0,0 +1,14 @@
+package org.koreait;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        new App(sc).run();
+
+        sc.close();
+    }
+}
